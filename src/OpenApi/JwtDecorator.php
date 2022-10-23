5113649c58566f6474d62c93664c3f5fa6dd402c
aymericcucherousset <aymeric.cucherousset@gmail.com>
2022-10-23 11:55:36 +0200

    Editing username property

--- src/OpenApi/JwtDecorator.php
+++ src/OpenApi/JwtDecorator.php
@@ -29,7 +29,7 @@ final class JwtDecorator implements OpenApiFactoryInterface
         $schemas['Credentials'] = new \ArrayObject([
             'type' => 'object',
             'properties' => [
-                'email' => [
+                'username' => [
                     'type' => 'string',
                     'example' => 'johndoe@example.com',
                 ],
