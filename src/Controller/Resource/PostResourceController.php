050b2fee7959fa8e62c02a3b991a831af1371b9f
aymericcucherousset <aymeric.cucherousset@gmail.com>
2023-01-10 22:22:25 +0100

    Adding post Resource with current user logged

--- src/Controller/Resource/PostResourceController.php
+++ src/Controller/Resource/PostResourceController.php
@@ -5,24 +5,31 @@ namespace App\Controller\Resource;
 use App\Entity\Resource;
 use App\Repository\ResourceRepository;
 use App\Service\ResourceManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpKernel\Attribute\AsController;
 
 #[AsController]
 class PostResourceController extends AbstractController
 {
-    public function __construct(private ResourceRepository $resourceRepository, private ResourceManager $resourceManager)
-    {
-    }
+    public function __construct(
+        private ResourceRepository $resourceRepository, 
+        private ResourceManager $resourceManager,
+        private EntityManagerInterface $entityManager
+    ){}
 
     public function __invoke(Resource $data): ?Resource
     {
         // If the user is null ask jwt to throw an exception
         $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
 
-        // Adding the default validation state to the new resource
+        // Adding the user logged in & the default validation state to the new resource
+        $data->setUser($this->getUser());
         $data->addValidationState($this->resourceManager->createDefaultValidationState($data));
 
+        // Persisting the new resource
+        $this->entityManager->persist($data);
+        $this->entityManager->flush();
         return $data;
     }
 }
