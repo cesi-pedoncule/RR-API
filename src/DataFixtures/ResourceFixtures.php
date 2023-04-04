c1b6a73e8a665e5c4389f48875aae4a27100576b
aymericcucherousset <aymeric.cucherousset@gmail.com>
2023-04-04 14:31:03 +0200

    Fix validations state

--- src/DataFixtures/ResourceFixtures.php
+++ src/DataFixtures/ResourceFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 use App\DataFixtures\UserFixtures;
 use App\Entity\Resource;
+use App\Entity\ValidationState;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
@@ -13,13 +14,20 @@ class ResourceFixtures extends Fixture implements DependentFixtureInterface
     {
         $faker = \Faker\Factory::create('fr_FR');
         // Create 20 fake resources
-        for ($i=0; $i < 25; $i++) { 
+        for ($i=0; $i < 25; $i++) {
+
+            $validationState = (new ValidationState())
+                ->setState($this->getReference('state_pending'))
+                ->setModerator($this->getReference('user_0'));
+
+            $manager->persist($validationState);
+
             $resource = (new Resource())
                 ->setTitle($faker->sentence(3))
                 ->setDescription($faker->paragraph(3))
                 ->addCategory($this->getReference('category_' . rand(0, 4)))
                 ->setIsPublic($i >= 20 ? false : true)
-                ->addValidationState($this->getReference('validationState_' . rand(1, 3)))
+                ->addValidationState($validationState)
                 ->setUser($this->getReference('user_' . rand(0, 9)));
             
             $manager->persist($resource);
